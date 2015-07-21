ff77154c5e4125505e4d71233f20e74912b0ed9a
Diederik Krols <diederik.krols@u2uconsult.com>
2015-07-21 20:51:48 +0200

    SplitView.

---
+++ XamlBrewer.Uwp.AdaptiveTriggerSample/Views/Shell.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using XamlBrewer.Uwp.AdaptiveTriggerSample.ViewModels;
+
+namespace XamlBrewer.Uwp.AdaptiveTriggerSample.Views
+{
+    public sealed partial class Shell : Page
+    {
+        public Shell()
+        {
+            this.InitializeComponent();
+
+            // Navigate to the first page (optionally)
+            var type = (DataContext as ShellViewModel).Menu.First().NavigationDestination;
+            SplitViewFrame.Navigate(type);
+
+            this.SizeChanged += Shell_SizeChanged;
+        }
+
+        private void Shell_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            WidthIndicator.Text = "<- " + e.NewSize.Width.ToString() + " ->";
+        }
+
+        private void Menu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count > 0)
+            {
+                var menuItem = e.AddedItems.First() as MenuItem;
+                if (menuItem.IsNavigation)
+                {
+                    SplitViewFrame.Navigate(menuItem.NavigationDestination);
+                }
+                else
+                {
+                    menuItem.Command.Execute(null);
+                }
+            }
+        }
+
+        private void SplitViewOpener_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+            if (e.Cumulative.Translation.X > 50)
+            {
+                MySplitView.IsPaneOpen = true;
+            }
+        }
+
+        private void SplitViewPane_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+            if (e.Cumulative.Translation.X < -50)
+            {
+                MySplitView.IsPaneOpen = false;
+            }
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+    }
+}
